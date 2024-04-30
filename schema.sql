896018e49223f6a5f1057970a238fbeb14114495
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-30 22:54:08 +0530

    database file

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE teacher;
+USE teacher;
+
+CREATE TABLE teacher_info;
+
+INSERT INTO teacher_info (
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    
+    
+)
